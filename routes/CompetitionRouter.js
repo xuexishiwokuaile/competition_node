4a4a563600d17cc3c3c6973cfae5d5f6d8f520c6
Anran Chen <1243099336@qq.com>
2021-03-31 20:02:07 +0800

    feat: 添加和更新时可以上传文件

--- routes/CompetitionRouter.js
+++ routes/CompetitionRouter.js
@@ -6,6 +6,8 @@
 import { Router ***REMOVED*** from "express";
 import CompetitionService from "../service/CompetitionService.js";
 import TypeService from "../service/TypeService.js";
+import { putStream ***REMOVED*** from "../util/oss.js";
+import formidable from "formidable";
 
 const router = Router();
 const competitionService = new CompetitionService();
@@ -13,43 +15,55 @@ const typeService = new TypeService();
 
 ***REMOVED****
  * @description 添加竞赛
- * @param {name, url, detail, image, type[]***REMOVED***
+ * @param {name, url, detail, image, type***REMOVED*** // type之间用逗号分隔 image格式为file
  * @url /competition/add
  * @return {***REMOVED***
 ***REMOVED***
 router.post("/add", async function (req, res, next) {
     // 从cookie中获取当前登录教师的id
     const teaId = req.signedCookies.id;
-    // 获取传递的参数
-    const competition = req.body;
-    // 获取当前时间
-    const date = new Date();
+    // 获取传递的参数，其中包含图片，格式为form-data
+    const form = new formidable.IncomingForm();
+    form.parse(req, async function (err, fields, files) {
+        if (err) throw err;
+        // 将文件上传到oss
+        const img = await putStream(files.image);
+        // 如果没有上传文件，url则为空，否则为文件的oss地址
+        const url = img ? img.url : null;
+        // 获取参数
+        const competition = fields;
+        // 获取当前时间
+        const date = new Date();
+        // 格式化type
+        const type = competition.type.split(",");
+
+    ***REMOVED***
+            const result = await competitionService.add({
+                name: competition.name,
+                url: competition.url,
+                detail: competition.detail,
+                image: url,
+                teaId: teaId,
+                date: date,
+            ***REMOVED***
+            // 添加竞赛类型
+            const types = await typeService.add({
+                comId: result,
+                type: type,
+            ***REMOVED***
+            res.json({
+                code: "0",
+                msg: "添加成功",
+                id: result,
+                type: types,
+            ***REMOVED***
+***REMOVED*** catch (e) {
+            res.json({
+                code: "1",
+                msg: e.name + ": " + e.message,
+            ***REMOVED***
 ***REMOVED***
-        const result = await competitionService.add({
-            name: competition.name,
-            url: competition.url,
-            detail: competition.detail,
-            image: competition.image,
-            teaId: teaId,
-            date: date,
-        ***REMOVED***
-        // 添加竞赛类型
-        const types = await typeService.add({
-            comId: result,
-            type: competition.type,
-        ***REMOVED***
-        res.json({
-            code: "0",
-            msg: "添加成功",
-            id: result,
-            type: types,
-        ***REMOVED***
-***REMOVED***
-        res.json({
-            code: "1",
-            msg: e.name + ": " + e.message,
-        ***REMOVED***
-***REMOVED***
+    ***REMOVED***
 ***REMOVED***
 
 ***REMOVED****
@@ -61,6 +75,7 @@ router.post("/add", async function (req, res, next) {
 router.delete("/delete", async function (req, res, next) {
     // 获取传递的参数
     const competition = req.query;
+
 ***REMOVED***
         const result = await competitionService.delete(competition);
         res.json({
@@ -77,40 +92,52 @@ router.delete("/delete", async function (req, res, next) {
 
 ***REMOVED****
  * @description 更新竞赛
- * @param {id, name, url, detail, image, teaId, type[]***REMOVED***
+ * @param {id, name, url, detail, image, teaId, type***REMOVED*** // type之间用逗号分隔 image格式为file
  * @url /competition/update
  * @return {***REMOVED***
 ***REMOVED***
 router.put("/update", async function (req, res, next) {
     // 从cookie中获取当前登录教师的id
     const teaId = req.signedCookies.id;
-    // 获取传递的参数
-    const competition = req.body;
+    // 获取传递的参数，其中包含图片，格式为form-data
+    const form = new formidable.IncomingForm();
+    form.parse(req, async function (err, fields, files) {
+        if (err) throw err;
+        // 将文件上传到oss
+        const img = await putStream(files.image);
+        // 如果没有上传文件，url则为空，否则为文件的oss地址
+        const url = img ? img.url : null;
+        // 获取参数
+        const competition = fields;
+        // 格式化type
+        const type = competition.type.split(",");
+
+    ***REMOVED***
+            const result = await competitionService.update({
+                id: competition.id,
+                name: competition.name,
+                url: competition.url,
+                detail: competition.detail,
+                image: url,
+                teaId: teaId,
+            ***REMOVED***
+            // 更新竞赛种类
+            const types = await typeService.update({
+                comId: competition.id,
+                type: type,
+            ***REMOVED***
+            res.json({
+                code: "0",
+                msg: result,
+                type: types,
+            ***REMOVED***
+***REMOVED*** catch (e) {
+            res.json({
+                code: "1",
+                msg: e.name + ": " + e.message,
+            ***REMOVED***
 ***REMOVED***
-        const result = await competitionService.update({
-            id: competition.id,
-            name: competition.name,
-            url: competition.url,
-            detail: competition.detail,
-            image: competition.image,
-            teaId: teaId,
-        ***REMOVED***
-        // 更新竞赛种类
-        const types = await typeService.update({
-            comId: competition.id,
-            type: competition.type,
-        ***REMOVED***
-        res.json({
-            code: "0",
-            msg: result,
-            type: types,
-        ***REMOVED***
-***REMOVED***
-        res.json({
-            code: "1",
-            msg: e.name + ": " + e.message,
-        ***REMOVED***
-***REMOVED***
+    ***REMOVED***
 ***REMOVED***
 
 ***REMOVED****
@@ -169,7 +196,7 @@ router.get("/findAllByDate", async function (req, res, next) {
  * @url /competition/findAllByHot
  * @return {competition[]***REMOVED***
 ***REMOVED***
-router.get("/findAllByHot",async function(req,res,next) {
+router.get("/findAllByHot", async function (req, res, next) {
     res.send(await competitionService.findAllByHot());
 ***REMOVED***
 
